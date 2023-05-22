b1c53bd8a6106f72b40244a74a1bb857274912bf
TigasMoro <122630395+TigasMoro@users.noreply.github.com>
2023-05-22 11:10:02 -0300

    Add files via upload

---
+++ javascript/aula13/dowhile.js
@@ -0,0 +1,5 @@
+var vezes = 1 
+do{
+    console.log(`Passo ${vezes}`)
+    vezes++
+} while(vezes <= 10)
---
+++ javascript/aula13/while.js
@@ -0,0 +1,5 @@
+var vezes = 1
+while(vezes <= 300){
+    console.log(`Passo ${vezes}`)
+    vezes++
+}

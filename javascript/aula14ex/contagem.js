4b35a22f0bf750ef23fcde40bf23752a79185e60
TigasMoro <122630395+TigasMoro@users.noreply.github.com>
2023-05-23 13:55:10 -0300

    Add files via upload

---
+++ javascript/aula14ex/contagem.js
@@ -0,0 +1,4 @@
+var inicio = 5, fim = 20, passo = 2
+for(inicio; inicio <= fim; inicio = passo + inicio){
+    console.log(`${inicio}`)
+}
---
+++ javascript/aula14ex/tabuada.js
@@ -0,0 +1,5 @@
+var tabu = 5, mult = 0, resultado
+for(mult;mult < 11; mult++){
+    resultado = tabu * mult
+    console.log(`${tabu} x ${mult} = ${resultado}`)    
+}
